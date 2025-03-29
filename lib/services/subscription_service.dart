8b34f6f6df26b9db4fca309f5ede9904fb9c8b86
MuhammadBilalEllahi <bilalillahi25@gmail.com>
2025-03-29 19:37:29 +0500

    Update getStripePlans method in SubscriptionService to fetch subscription status based on user subscription; comment out unused createSubscription method for clarity and maintainability.

--- lib/services/subscription_service.dart
+++ lib/services/subscription_service.dart
@@ -6,23 +6,14 @@ class SubscriptionService {
 
   Future<List<dynamic>> getStripePlans() async {
     try {
-      final response = await _apiService.get('/stripe/prices');
-      return response;
+      final response = await _apiService.get('/stripe/prices/checkUserSubscribe');
+      return response.data;
+      // returns {data: [], message: 'notSubscribed'} or {data: [], message: 'alreadySubscribed'}
     } catch (e) {
       throw Exception('Failed to fetch plans: $e');
     }
   }
 
-  // Future<dynamic> createSubscription(String priceId) async {
-  //   try {
-  //     final response = await _apiService.post('/stripe/create-subscription', {
-  //       'priceId': priceId,
-  //     });
-  //     return response;
-  //   } catch (e) {
-  //     throw Exception('Failed to create subscription: $e');
-  //   }
-  // }
 
   Future<dynamic> getSubscriptionStatus() async {
     try {
